c317048d7c66237a584ff3a4337089418cbe6eed
Laurence Liang <lliang@groq.com>
2025-05-19 23:50:27 -0400

    chore: wip

--- scripts/experiments/run_batch_1.sh
+++ scripts/experiments/run_batch_1.sh
@@ -1,2 +1,6 @@
-uv run src/cpca_exp/run_experiment.py --task-id 53 --output-filepath "outfile-task-53.yaml" --ndim 10
+uv run src/cpca_exp/run_experiment.py --task-id 11 --output-filepath "outfile-task-11-ndim-2.yaml" --ndim 2
+uv run src/cpca_exp/run_experiment.py --task-id 167140 --output-filepath "outfile-task-167140-ndim-2.yaml" --ndim 2
+uv run src/cpca_exp/run_experiment.py --task-id 53 --output-filepath "outfile-task-53-ndim-2.yaml" --ndim 2
+uv run src/cpca_exp/run_experiment.py --task-id 3560 --output-filepath "outfile-task-3560-ndim-2.yaml" --ndim 2
+uv run src/cpca_exp/run_experiment.py --task-id 3573 --output-filepath "outfile-task-3573-ndim-2.yaml" --ndim 2
 
